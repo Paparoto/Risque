2f5c5dce617210a8af03f940edc07773c859af58
Melvin <124679142+Didros75@users.noreply.github.com>
2024-05-30 16:50:39 +0200

    Update host.py

--- host.py
+++ host.py
@@ -3,9 +3,6 @@ import threading
 
 clients = []
 
-"""def get_client_number(conn):
-    return clients.index(conn)+1"""
-
 def broadcast(message, conn):
     for client in clients:
         if client != conn:
@@ -14,8 +11,11 @@ def broadcast(message, conn):
 def handle_client(conn):
     while True:
         try:
-
             message = conn.recv(1024)
+            msg=message.decode("utf-8")
+            msg="Client" +str(clients.index(conn)+1) + " : " +msg
+            message=msg.encode("utf-8")
+            print(msg)
 
             if message:
                 broadcast(message, conn)
@@ -30,6 +30,7 @@ def handle_client(conn):
 
 def main():
     host = socket.gethostname()
+    print(host)
     port = 50000
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -41,6 +42,10 @@ def main():
     while True:
         conn, addr = server.accept()
         clients.append(conn)
+        acceuil = "Vous etes client" + str(clients.index(conn)+1)
+        acceuil=acceuil.encode("utf-8")
+        conn.send(acceuil)
+
         print(f"Connection from {addr}, client{clients.index(conn)+1}")
         thread = threading.Thread(target=handle_client, args=(conn,))
         thread.start()
