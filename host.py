275b5ccd7e684594fe80e3a9eca5c2028dbca9c7
Melvin <124679142+Didros75@users.noreply.github.com>
2024-05-31 22:05:49 +0200

    Update host.py

--- host.py
+++ host.py
@@ -2,44 +2,7 @@ import socket
 import threading
 
 clients = []
-
-class Pays:
-    def __init__(self, nom, liste_adjacents, position:tuple, troupes:int, rendement:int, couleur="blanc"):
-        self.nom = nom
-        self.liste_adjacents = liste_adjacents
-        self.position = position
-        self.troupes = troupes
-        self.rendement = rendement
-        self.couleur=couleur
-
-    def __str__(self):
-        return self.nom
-
-    def deplacement(self, autre_pays):
-        if autre_pays not in self.liste_adjacents :
-            return "impossible"
-        elif autre_pays.couleur == self.couleur:
-            autre_pays.troupes+= self.troupes
-            self.troupes=0
-        elif self.troupes<autre_pays.troupes:
-            autre_pays.troupes -= self.troupes
-            self.troupes = 0
-
-        elif self.troupes>autre_pays.troupes:
-            autre_pays.couleur = self.couleur
-            autre_pays.troupes = self.troupes - autre_pays.troupes
-            self.troupes=0
-
-        elif self.troupes == autre_pays.troupes:
-            self.troupes = 0
-            autre_pays.troupes = 0
-
-    def augmenter_armée(self):
-        if self.couleur == "blanc":
-            return
-        else:
-            self.troupes += self.rendement
-
+pret=[]
 
 def broadcast(message, conn):
     for client in clients:
@@ -47,11 +10,20 @@ def broadcast(message, conn):
             client.send(message)
 
 def handle_client(conn):
+
     while True:
         try:
             message = conn.recv(1024)
             msg=message.decode("utf-8")
-            msg="Client" +str(clients.index(conn)+1) + " : " +msg
+            if msg=="pret":
+                pret.append(conn)
+
+            if len(pret)==2:
+                start = "start"
+                start = start.encode("utf-8")
+                for conn in clients:
+                    conn.send(start)
+
             message=msg.encode("utf-8")
             print(msg)
 
@@ -84,11 +56,11 @@ def main():
         acceuil=acceuil.encode("utf-8")
         conn.send(acceuil)
 
+
         print(f"Connection from {addr}, client{clients.index(conn)+1}")
         thread = threading.Thread(target=handle_client, args=(conn,))
         thread.start()
 
 if __name__ == "__main__":
     main()
-
 
